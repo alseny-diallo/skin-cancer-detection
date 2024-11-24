753d56df33684dbd33edb4c0c661cb69d8a17bfe
alseny-diallo <alsenydiallo0409@gmail.com>
2024-11-24 13:39:44 +0000

    implement Flask app for skin cancer detection

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request
-
 from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import load_img
-from tensorflow.keras.preprocessing import image
-
+from tensorflow.keras.utils import load_img, img_to_array
 import os
 import numpy as np
 
-
 app = Flask(__name__)
-model = load_model('model.h5')
 
+model = load_model('skin_cancer_model.h5')
+
+classes_list = [
+    'actinic keratosis',
+    'basal cell carcinoma',
+    'dermatofibroma',
+    'melanoma',
+    'nevus',
+    'seborrheic keratosis',
+    'squamous cell carcinoma',
+    'vascular lesion'
+]
 
 @app.route('/')
 def home():
     return render_template('main.html')
-
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -27,20 +33,18 @@ def predict():
             file_path = os.path.join('static/images', filename)
             file.save(file_path)
 
-        img = load_img(file_path, target_size=(300, 300))
-        x = image.img_to_array(img)/255
-        x = np.expand_dims(x, axis=0)
+            img = load_img(file_path, target_size=(224, 224))
+            x = img_to_array(img) / 255.0
+            x = np.expand_dims(x, axis=0)
 
-        images = np.vstack([x])
-        classes = model.predict(images)
+            # Prédiction
+            predictions = model.predict(x)
+            print (predictions)
+            predicted_class_index = np.argmax(predictions)
+            label = classes_list[predicted_class_index]
 
-        if classes[0] > 0.5:
-            label = "The Picture is a Pizza"
-        else:
-            label = "The Picture is a Pasta"
-
-        return render_template("result.html", label=label, file_path=file_path)
-
+            # Afficher le résultat
+            return render_template("result.html", label=label, file_path=file_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
