de7cc13d9c762ef8f4cb8a0c733cd574ad4ba674
alseny-diallo <alsenydiallo0409@gmail.com>
2024-11-26 15:09:36 +0000

    updating content

--- static/main.js
+++ static/main.js
@@ -1,29 +1,29 @@
-const img = document.getElementById("img");
-const fileNameLabel = document.getElementById("fileNameLabel");
+const img = document.getElementById('img');
+const fileNameLabel = document.getElementById('fileNameLabel');
 
 function file_img(event) {
-    let fileInput = event.target;
-    var file = fileInput.files[0];
-    return file;
+  let fileInput = event.target;
+  var file = fileInput.files[0];
+  return file;
 }
 
 function setUp(event) {
-    const file = file_img(event);
+  const file = file_img(event);
 
-    if (file) {
-        fileNameLabel.textContent = file.name;
-    }
+  if (file) {
+    fileNameLabel.textContent = file.name;
+  }
 
-    if (file) {
-        const reader = new FileReader();
+  if (file) {
+    const reader = new FileReader();
 
-        reader.onload = function (event) {
-            img.style.display = "block";
-            img.src = event.target.result;
-        };
+    reader.onload = function (event) {
+      img.style.display = 'block';
+      img.src = event.target.result;
+    };
 
-        reader.readAsDataURL(file);
-    }
+    reader.readAsDataURL(file);
+  }
 }
 
-document.getElementById("inputPicture").addEventListener("change", setUp);
+document.getElementById('inputPicture').addEventListener('change', setUp);
